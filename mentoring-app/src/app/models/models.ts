173fd7d4aff81a98050be484516e5fe1e382aa07
mshepida <mhepida@gmail.com>
2019-10-13 18:47:28 +0300

    move classes to models

--- mentoring-app/src/app/models/models.ts
+++ mentoring-app/src/app/models/models.ts
@@ -11,3 +11,17 @@ export interface Course {
     first_name: string,
     last_name: string
   }
+
+  class TestCourse implements Course {
+    id: 1;
+    title: "Course 1";
+    duration: 32;
+    description: 'test course'
+    creation_date: '11 Sep 2016' 
+  }
+  
+  class TestUser implements User {
+    id: 1;
+    first_name: 'Alan';
+    last_name: 'Paul';
+  }
