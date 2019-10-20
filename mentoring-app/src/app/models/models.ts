ea12b953b75e10421c95244419bb80506c1b944d
mshepida <mshepida@gmail.com>
2019-10-20 19:54:18 +0300

    refactoring

--- mentoring-app/src/app/models/models.ts
+++ mentoring-app/src/app/models/models.ts
@@ -1,25 +1,7 @@
-  export interface Course {
-    id: number;
-    title: string;
-    creationDate: Date;
-    duration: number;
-    description: string;
-  }
-
   export interface User {
     id: number;
     firstName: string;
     lastName: string;
-  }
-
-  export class CourseClass implements Course {
-    constructor(
-      public id: number,
-      public title: string,
-      public duration: number,
-      public description: string,
-      public creationDate: Date
-    ) {}
   }
 
   export class UserClass implements User {
